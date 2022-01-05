bf91552e64430787fe462d627da499fb49e95eca
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-01-05 10:24:29 +0900

    select1

---
+++ 3/select.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	
+	start := time.Now()
+	c := make(chan interface{})
+
+	go func() {
+		time.Sleep(5*time.Second)
+		close(c)
+	}()
+
+	fmt.Println("Blocking on read,,")
+	select{
+	case <- c:
+		fmt.Println("Unblocked",time.Since(start),"lataer.")
+	}
+}
