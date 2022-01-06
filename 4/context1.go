3c3f17a48390267bc65197fff55eb0476503cbd0
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-01-06 17:04:34 +0900

    contextWithValueの安全な使い方

--- 4/context1.go
+++ 4/context1.go
@@ -5,16 +5,31 @@ import (
 	"fmt"
 )
 
+type ctxKey int
+
+const (
+	ctxUserID ctxKey = iota
+	ctxAuthToken
+)
+
+func UserID(c context.Context) string {
+	return c.Value(ctxUserID).(string)
+}
+
+func AuthTOken(c context.Context) string {
+	return c.Value(ctxAuthToken).(string)
+}
+
 func main() {
-	ProcessRequest("jane","abc123")
+	ProcessRequest("jane", "abc123")
 }
 
 func ProcessRequest(userID string, authToken string) {
-	ctx := context.WithValue(context.Background(),"userID",userID)
-	ctx = context.WithValue(ctx, "authToken",authToken)
+	ctx := context.WithValue(context.Background(), ctxUserID, userID)
+	ctx = context.WithValue(ctx, ctxAuthToken, authToken)
 	HandleRespone(ctx)
 }
 
 func HandleRespone(ctx context.Context) {
-	fmt.Println("Handling respons for ",ctx.Value("userID"),ctx.Value("authToken"))
+	fmt.Println("Handling respons for ", UserID(ctx), AuthTOken(ctx))
 }
