bc88cf7e1dd869ea0b9ae2132dbad1d1abc524ba
xxarupakaxx <arupaka3tdigks@gmail.com>
2022-01-07 15:03:01 +0900

    流量制限をかける

--- 5/burst.go
+++ 5/burst.go
@@ -2,24 +2,34 @@ package main
 
 import (
 	"context"
+	"golang.org/x/time/rate"
 	"log"
 	"os"
 	"sync"
 )
 
 func Open() *APIConnection {
-	return &APIConnection{}
+	return &APIConnection{
+		rateLimiter: rate.NewLimiter(rate.Limit(1),1),
+	}
 }
 
 type APIConnection struct {
+	rateLimiter *rate.Limiter
 
 }
 
 func (a *APIConnection) ReadFile(ctx context.Context) error {
+	if err := a.rateLimiter.Wait(ctx); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (a *APIConnection) ResolveAddress(ctx context.Context) error {
+	if err := a.rateLimiter.Wait(ctx); err != nil {
+		return err
+	}
 	return nil
 }
 
